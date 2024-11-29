2fbb595bb815c903f9b2c1afa44d65e54dbcb5fa
Fa Li <lifa.lbnl@gmail.com>
2024-11-28 16:15:28 -0800

    Update methane_analysis_demo.py

--- Analysis/methane_analysis_demo.py
+++ Analysis/methane_analysis_demo.py
@@ -1,42 +1,6 @@
 from netCDF4 import Dataset
 import copy
 from methane_analysis_toolbox import FCH4_anomaly_calculation,Forcing_anomaly_calculation,LinearReg_model,ForcingData_load,FCH4Data_load,FCH4_anomaly_yearly,Forcing_anomaly_yearly
-
-def grid_variation_analysis(max_lat_idx,dir_forcing,dir_fch4,save_file,forcing_file_list,forcing_vars,target_var):
-    #load FCH4 upscaled data (start from the year of 2002)
-    fch4_var,lats,lons=FCH4Data_load(dir_fch4,max_lat_idx)
-    #obtain FCH4 anomaly
-    fch4_var_anomaly=FCH4_anomaly_calculation(fch4_var,lats,lons)
-    #obatain datsets of input forcing (TS, TA, SC, PA, P, SWC, WS, and GPP)
-    all_forcing_vars=ForcingData_load(forcing_file_list,forcing_vars,dir_forcing,max_lat_idx,lats,lons)
-    #obtain forcing anomaly
-    all_forcing_vars_anomaly=Forcing_anomaly_calculation(all_forcing_vars,lats,lons)
-
-    #update forcing_var and all_vars
-    forcing_vars.extend(['gpp'])
-    all_vars=copy.deepcopy(forcing_vars)
-    all_vars.extend(['fch4'])
-
-    #calculate partial correlation between FCH4 and its drivers in each grid
-    partial_result=Par_cor(forcing_vars,lats,lons,all_forcing_vars_anomaly,fch4_var_anomaly,all_vars,target_var)
-
-    #obtain and save the results
-    nc_fid2 = Dataset(save_file, 'w', format="NETCDF4")
-    nc_fid2.createDimension('latitude', len(lats))
-    nc_fid2.createDimension('longitude', len(lons))
-    nc_fid2.createDimension('forcing_id', partial_result.shape[0])
-    latitudes = nc_fid2.createVariable('latitude', 'f4', ('latitude',))
-    longitudes = nc_fid2.createVariable('longitude', 'f4', ('longitude',))
-    forcing_var_id = nc_fid2.createVariable("forcing_id", "f4", ("forcing_id",))
-    par_cor_results = nc_fid2.createVariable('par_r', "f4", ("forcing_id","latitude", "longitude",), zlib=True)
-    forcing_var_id[:] = range(partial_result.shape[0])
-    forcing_var_id.units='_'.join(forcing_vars)
-    latitudes[:] = lats[:]
-    longitudes[:] = lons[:]
-    par_cor_results[:] = partial_result[:]
-    nc_fid2.close()
-
-
 def grid_trend_analysis(forcing_group,min_lat_idx,max_lat_idx,dir_forcing,dir_fch4,forcing_file_list,forcing_vars,save_file):
     # load FCH4 upscaled data
     fch4_var, lats, lons = FCH4Data_load(dir_fch4, min_lat_idx,max_lat_idx)
